668be3d3080a6b2175baeae3a6057ac59dfa1212
Motorin Aleksey <aleksey.motorin2015@yandex.ru>
2022-04-04 09:05:06 +0300

    Initial Commit

---
+++ Places/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Places
+//
+//  Created by Алексей Моторин on 04.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
